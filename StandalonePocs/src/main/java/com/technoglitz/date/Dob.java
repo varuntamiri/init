406785c0acf7646e198758c1a23822d1c407297b
varuntamiri <tamiri.varun@gmail.com>
2016-08-02 19:05:09 +0800

    Experimenting with Java 8 Date API

--- StandalonePocs/src/main/java/com/technoglitz/date/Dob.java
+++ StandalonePocs/src/main/java/com/technoglitz/date/Dob.java
@@ -1,13 +1,25 @@
 package com.technoglitz.date;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import static java.time.Instant.now;
+import static java.time.LocalDate.of;
+import static java.time.Period.between;
+import static java.time.ZoneId.systemDefault;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
 
 public class Dob {
+	private static final Instant fixdIns = Instant.parse("2010-01-01T12:00:00Z");
+	private static final Clock fixed = Clock.fixed(Dob.fixdIns, systemDefault());//Fixing the clocks to Jan 01, 2010
 
 	public static void main(String[] args) {
-		 LocalDate birthday = LocalDate.of(1992, 07, 19);
-		    long yearsDelta = birthday.until(LocalDate.now(), ChronoUnit.YEARS);
-		    System.out.println("yearsDelta = " + yearsDelta);
+		
+		Dob dob = new Dob();
+		System.out.println("Age is  = " + dob.getAge(1985, 10, 04));
+	}
+	
+	public int getAge(int year,int month, int day) {
+		return between(of(year, month, day), LocalDateTime.ofInstant(now(Dob.fixed), systemDefault()).toLocalDate()).getYears();
 	}
 }
